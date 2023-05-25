deb45733b5bf9563fcf19875faf6c58396a1e0fc
B1ACK917 <397615103@qq.com>
2023-05-25 11:06:22 +0800

    update: aws const

---
+++ src/router/consts_aws.js
@@ -0,0 +1,10 @@
+export const queryLoadTemplate = "http://18.162.45.61:23457/getTemplate";
+export const queryLoadHDFSList = "http://18.162.45.61:23457/getHDFSTableList";
+export const querySubmitTask = "http://18.162.45.61:23457/submitTask";
+export const queryRunTask = "http://18.162.45.61:23457/runTask";
+export const queryTaskStatus = "http://18.162.45.61:23457/taskStatus";
+export const queryGetTaskData = "http://18.162.45.61:23457/getTaskData";
+export const queryDownloadTaskData = "http://18.162.45.61:23457/downloadTaskData";
+export const querySearchPatent = "http://18.162.45.61:23457/patentSearch";
+
+export const queryTaskStatusGap = 5000;
